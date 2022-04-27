e3c6dc692a286e23e603e2f7aad750d8defc4534
mzmarlies <marliesclaire@gmail.com>
2022-04-27 16:15:53 -0600

    testing a push to main

--- src/components/TileOne.js
+++ src/components/TileOne.js
@@ -1,6 +1,7 @@
 const TileOne = (props) => {
     return(
         <section>
+            <h2>this is a test</h2>
                 <>
                 {
                     props.photos.map(singlePhoto => {

080cc38db4b0b33be5d68d5fda9b95d5e8c4831f
mzmarlies <marliesclaire@gmail.com>
2022-04-27 16:06:00 -0600

    first push to production

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Modules:
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+// Components:
+import TileOne from './components/TileOne';
+// import TileTwo from './components/TileTwo';
+
+
 function App() {
+  const [allPhotos, setAllPhotos] = useState([]);
+  // const [filteredPhotos, setFilteredPhotos] = useState([])
+
+  useEffect (() => {
+    axios({
+      url: 'https://api.unsplash.com/search/photos',
+      method:'GET',
+      dataResponse:'json',
+      params: {
+        client_id: '3-B5WhPSmJkkSBV3G3AWDnf1wcdjZSPALyHjFShogn4',
+        query:'puppies',
+        per_page: 2
+      }
+    }).then(response => {
+      // console.log(response);
+        const responsedata = response.data.results
+        console.log('responsedata', responsedata)
+
+        setAllPhotos(responsedata);
+    })
+  }, []);
+
+  // const getPhotos = (event, orientationChoice) => {
+  //   event.preventDefault();
+
+  //   const copyOfPhotos = [...allPhotos]
+  //   const photosFiltered = copyOfPhotos.filter(singlePhoto => {
+  //     return singlePhoto.orientation === orientationChoice
+  //   })
+
+  //   setFilteredPhotos(photosFiltered)
+  // }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory Game</h1>
+      <div className="results">
+        <TileOne photos={allPhotos} />
+        {/* <TileTwo photos={allPhotos} /> */}
+      </div>
     </div>
   );
 }
